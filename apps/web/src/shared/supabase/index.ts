bba2cd2e040950cf7750ffbd7f59d46ce71b325a
chernenkoik <chernenkoik@tutu.tech>
2025-07-27 10:52:25 -0300

    fix build

--- apps/web/src/shared/supabase/index.ts
+++ apps/web/src/shared/supabase/index.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://mjnbydawpuhhrbwsqqkd.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qbmJ5ZGF3cHVoaHJid3NxcWtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NjkxODEsImV4cCI6MjA2OTE0NTE4MX0.S05a6H-V9X2-yHHYYkHrJdqxFuxbiR3tplZwzNSUFdw";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
