01dbfa1ebebc4bba1bcb95ceee45773c9f2b8804
chernenkoik <chernenkoik@tutu.tech>
2025-07-27 10:36:28 -0300

    fix build

--- apps/web/src/shared/supabase/index.ts
+++ apps/web/src/shared/supabase/index.ts
@@ -4,7 +4,3 @@ const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qbmJ5ZGF3cHVoaHJid3NxcWtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NjkxODEsImV4cCI6MjA2OTE0NTE4MX0.S05a6H-V9X2-yHHYYkHrJdqxFuxbiR3tplZwzNSUFdw";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-
-supabase.auth.signUp({
-  email: "",
-});
